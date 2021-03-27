56b40bc509a7029dce9dc140a9462e692d09ac0b
alvif <asm240800@gmail.com>
2021-03-27 13:24:41 +0700

    split POST string success :)

--- app/model/model_students.py
+++ app/model/model_students.py
@@ -18,21 +18,25 @@ class ModelStudents:
 
     def create(self, input_data):
         try:
-            tmpp = input_data.split('\r\n\r\n',1)[1]
+            # split unused string data
+            tmpp = input_data.split('\r\n\r\n', 1)[1]
+            # split nim, nama, and angkatan
             tmp = tmpp.split('&')
             tmp1 = {}
+            # open json data
             with open('db/data.json') as f:
                 data_raw = json.load(f)
                 data = data_raw
+            # add new data from splitted POST string
             for d in tmp:
                 tmp1[d.split('=')[0]] = d.split('=')[1]
             data['mahasiswa'].append(tmp1)
+            # write to json
             with open('db/data.json', 'w') as wf:
                 json.dump(data, wf)
             print('data successfully added!')
         except Exception as e:
             print(e)
-
 
     def update(self, data):
         pass
